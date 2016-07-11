e1b7bb0f4d5efe1a718766e2f227d4151a6238ab
Mischa ter Smitten <mischa@tersmitten.nl>
2016-07-11 16:16:45 +0200

    Consistency changes
    * Ansible 2 fixes
    * Add support for 16.04

--- tests/vagrant.sh
+++ tests/vagrant.sh
@@ -27,8 +27,18 @@ echo 'mysql-server-5.1 mysql-server/root_password password vagrant' | debconf-se
 echo 'mysql-server-5.1 mysql-server/root_password_again password vagrant' | debconf-set-selections;
 echo 'mysql-server-5.5 mysql-server/root_password password vagrant' | debconf-set-selections;
 echo 'mysql-server-5.5 mysql-server/root_password_again password vagrant' | debconf-set-selections;
+echo 'mysql-server-5.6 mysql-server/root_password password vagrant' | debconf-set-selections;
+echo 'mysql-server-5.6 mysql-server/root_password_again password vagrant' | debconf-set-selections;
 
-apt-install mysql-server;
+echo 'percona-server-server-5.6 percona-server-server/root_password password vagrant' | debconf-set-selections;
+echo 'percona-server-server-5.6 percona-server-server/root_password_again password vagrant' | debconf-set-selections;
+
+# No 5.7 support in common_schema yet, no 5.6 support in 16.04
+if $(lsb_release -r | grep -q '16.04'); then
+  apt-install percona-server-server-5.6;
+else
+  apt-install mysql-server;
+fi
 
 cat << EOF > ~/.my.cnf
 [client]
